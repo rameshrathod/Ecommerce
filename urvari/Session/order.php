2bc3fcc4e97935515e8342b175b2a0bade04f16d
rameshrathod <rameshrathod1194@gmail.com>
2016-06-15 10:29:32 +0530

    urvari final commit

---
+++ urvari/Session/order.php
@@ -0,0 +1,29 @@
+
+<?php
+// Start the session
+session_start();
+?>
+<?php
+include "logout.html";
+echo "USERNAME " . $_SESSION["user"] . ".<br>";
+echo "PASSWORD " . $_SESSION["password"] . ".";
+
+	
+
+ if ($_SESSION['timeout'] + 1 * 60 < time()) 
+ {
+   // remove all session variables
+   session_unset();
+   // destroy the session 
+   session_destroy(); 
+     echo" session timed out";
+	header('Location: main.php');
+  } else
+  {
+     echo "session ok";
+  }
+		
+echo "welcome to do order";
+
+
+?>
